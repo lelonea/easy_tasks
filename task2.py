c35e61f554596ff92cfbbd101e0e1f925708759e
Nastya <leloneya@gmail.com>
2021-05-28 17:32:17 +0300

    Task is done. Created function, that returns first repeating of element in list

--- task2.py
+++ task2.py
@@ -1,11 +1,15 @@
 """As second task: you have the list of symbols (example: a, b, c, c, a (it’s very primitive)).
 You should write function that returns the first repeating element from this list (in our example it’s “c”)."""
 
-
 list_to_check = ['a', 'd', 'b', 'c', 'd', 'a', 'd']
 
 
 def first_repeating(list_of_symbols):
+    """
+    Function to find first repeating of elements in list
+    :param list_of_symbols: list with symbols
+    :return: first repeating element if there is any
+    """
     seen = []
     for symbol in list_of_symbols:
         if symbol in seen:
