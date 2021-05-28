6a5584844e3f9cfe8fc256c489d20a8214981a85
Nastya <leloneya@gmail.com>
2021-05-28 17:21:19 +0300

    first commit

---
+++ task2.py
@@ -0,0 +1,18 @@
+"""As second task: you have the list of symbols (example: a, b, c, c, a (it’s very primitive)).
+You should write function that returns the first repeating element from this list (in our example it’s “c”)."""
+
+
+list_to_check = ['a', 'd', 'b', 'c', 'd', 'a', 'd']
+
+
+def first_repeating(list_of_symbols):
+    seen = []
+    for symbol in list_of_symbols:
+        if symbol in seen:
+            return f'First repeating element is {symbol}'
+        else:
+            seen.append(symbol)
+    return 'There are no repeating elements in this list'
+
+
+print(first_repeating(list_to_check))
